cdb9f2e13b6b54f602889f2bcde69ea843f0e61e
ori-volfo <ori.volfo@gmail.com>
2017-12-26 19:16:11 +0200

    components + service

---
+++ app/course.service.ts
@@ -0,0 +1,6 @@
+
+export class CourseService{
+    getCourses(): string[]{
+        return ["Course1","Course3","Course3"];
+    }
+}
---
+++ app/courses.component.ts
@@ -0,0 +1,24 @@
+import {Component} from "angular2/core"
+import {CourseService} from './course.service'
+
+@Component({
+    selector: 'courses',
+    template: `<h2>Courses</h2>
+    {{title}}
+        <ul>
+            <li *ngFor="#course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `,
+    providers: [CourseService]
+})
+export class CoursesComponent{
+    title: string = "This is my title";
+    courses;
+
+    constructor(courseService: CourseService){
+        this.courses = courseService.getCourses();
+    }
+
+}
